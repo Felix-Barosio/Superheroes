8ca420d599d02b058278ad2e5ede0fab13431a1a
Felix <barosiofelix@gmail.com>
2022-12-03 09:41:14 +0300

    Added validations

--- app/models/power.rb
+++ app/models/power.rb
@@ -3,4 +3,7 @@ class Power < ApplicationRecord
     has_many :hero_powers
     has_many :heros, through: :hero_powers
     
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true, length: { minimum: 20 }
+
 end
