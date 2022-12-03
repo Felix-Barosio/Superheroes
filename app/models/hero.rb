2a99c5bc70999d99589cf1369c82f71a0e5f7aa6
Felix <barosiofelix@gmail.com>
2022-12-03 09:39:57 +0300

    Added validations

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,4 +3,7 @@ class Hero < ApplicationRecord
     has_many :hero_powers
     has_many :powers, through: :hero_powers
 
+    validates :name, presence: true, uniqueness: true
+    validates :super_name, presence: true, uniqueness: true
+
 end
