9e0bacb024b2bfe4cc50d73d82962de7a0dc095c
Felix <barosiofelix@gmail.com>
2022-12-03 16:34:25 +0300

    Added dependent destroy

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,6 @@
 class Hero < ApplicationRecord
 
-    has_many :hero_powers
+    has_many :hero_powers, dependent: :destroy
     has_many :powers, through: :hero_powers
 
     validates :name, presence: true, uniqueness: true
