2af9daa4647648a482a1209afe6f2e72b35a8f3a
Felix <barosiofelix@gmail.com>
2022-12-03 09:03:40 +0300

    Added GET /heroes, GET /heroes/:id

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,19 @@
 class HeroesController < ApplicationController
+
+    def index
+        render json: Hero.all
+    end
+
+    def show
+        hero = find_hero
+        render json: hero, status: :ok, serializer: ShowHeroPowerSerializer
+    end
+
+
+    private
+
+    def find_hero
+        Hero.find(params[:id])
+    end
+
 end
