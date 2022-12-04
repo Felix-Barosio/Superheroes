4e93daddbc33b7e4b4fd5908ec3d5a3a8690b388
Felix <barosiofelix@gmail.com>
2022-12-04 17:51:17 +0300

    Added PATCH & DELETE

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,6 +1,7 @@
 class HeroPowersController < ApplicationController
 
     # Error handling
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # POST /hero_powers
@@ -9,12 +10,36 @@ class HeroPowersController < ApplicationController
         render json: hero_power, status: :created
     end
 
+    # PATCH /hero_powers/:id
+    def update
+        hero_power = find_hero_power
+        hero_power.update!(hero_power_params)
+        render json: hero_power, status: :ok
+    end
+
+    # DELETE /hero_powers/:id
+    def destroy
+        hero_power = find_hero_power
+        hero_power.destroy
+        render json: { message: 'Hero Power has been deleted' }, status: :ok
+    end
+
 
     private
+
+    # Finding hero_power by id
+    def find_hero_power
+        HeroPower.find(params[:id])
+    end
 
     # paramiters to create a new hero_power
     def hero_power_params
         params.permit(:strength, :hero_id, :power_id)
+    end
+
+    # hero_power not found response
+    def render_not_found_response
+        render json: { errors: "Hero Power not found"}, status: :not_found
     end
 
     # render messages for invalid responses
