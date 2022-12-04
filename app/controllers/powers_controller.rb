3e430130f8ee8810a369d53a6cd1356b406cd761
Felix <barosiofelix@gmail.com>
2022-12-04 09:33:57 +0300

    Added POST and PATCH

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,6 +1,7 @@
 class PowersController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # GET /powers
     def index
@@ -10,7 +11,20 @@ class PowersController < ApplicationController
     # GET /powers/:id
     def show
         power = find_power
-        render json: power, status: :ok, each_serializer: PowerSerializer
+        render json: power, status: :ok, serializer: PowerSerializer
+    end
+
+    # POST /powers
+    def create
+        power = Power.create!(power_params)
+        render json: power, status: :created
+    end
+
+    # PATCH /powers/:id
+    def update
+        power = find_power
+        power.update!(power_params)
+        render json: power, status: :ok
     end
 
     private
@@ -20,9 +34,19 @@ class PowersController < ApplicationController
         Power.find(params[:id])
     end
 
+    # atrributes paramiters for new power
+    def power_params
+        params.permit(:name, :description)
+    end
+
     # power not found response
     def render_not_found_response
         render json: { errors: "Power not found"}, status: :not_found
+    end
+
+    # render messages for invalid responses
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
