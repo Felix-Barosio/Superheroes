b739867a19e8dca0e8df3da5251795a2f68593fb
Felix <barosiofelix@gmail.com>
2022-12-04 17:43:07 +0300

    Added POST to hero_powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,25 @@
 class HeroPowersController < ApplicationController
+
+    # Error handling
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    # POST /hero_powers
+    def create
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: hero_power, status: :created
+    end
+
+
+    private
+
+    # paramiters to create a new hero_power
+    def hero_power_params
+        params.permit(:strength, :hero_id, :power_id)
+    end
+
+    # render messages for invalid responses
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
