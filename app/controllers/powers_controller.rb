3a8f04d8c53d40d73a138a8d9bd3121d095e8fc5
Felix <barosiofelix@gmail.com>
2022-12-04 09:21:49 +0300

    Added GET /powers, GET /powers/:id

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,28 @@
 class PowersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /powers
+    def index
+        render json: Power.all
+    end
+
+    # GET /powers/:id
+    def show
+        power = find_power
+        render json: power, status: :ok, each_serializer: PowerSerializer
+    end
+
+    private
+
+    # finding power by id
+    def find_power
+        Power.find(params[:id])
+    end
+
+    # power not found response
+    def render_not_found_response
+        render json: { errors: "Power not found"}, status: :not_found
+    end
+
 end
